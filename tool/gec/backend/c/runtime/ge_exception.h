4732beeb53263a15e337ee0d52d30121fb225174
Eric Bezault <ericb@gobosoft.com>
2024-05-13 13:07:34 +0200

    gec: added more debug instructions to figure out why we get a crash when creating SPECIAL objects on azure pipelines when compiled with zig.

--- tool/gec/backend/c/runtime/ge_exception.h
+++ tool/gec/backend/c/runtime/ge_exception.h
@@ -284,7 +284,7 @@ extern EIF_REFERENCE GE_check_void2(EIF_REFERENCE obj, EIF_INTEGER i);
  * If it is, then raise a no-more-memory exception.
  * Return `ptr'.
  */
-#define GE_null(ptr) GE_check_null(ptr)
+#define GE_null(ptr) (ptr)
 extern void* GE_check_null(void* ptr);
 
 /*
